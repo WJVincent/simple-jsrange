711b3191ae5790188dbfbfa305278ecf70a0d444
William Vincent <william@william-vincent.dev>
2021-06-18 22:00:46 -0400

    moved helper functions into seperate files

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+const util = require('util');
+
+const newRangeError = (...args) =>
+  new RangeError(`Invalid args for range: ${util.inspect(...args)}`)
+
+const throwInvalidRange = (...args) => {
+  throw newRangeError(...args);
+}
+const checkValue = (value) => {
+  const isNumber = typeof value === "number";
+  const isValidString = typeof value === "string" && value !== " " && value.length === 1;
+  return isNumber || isValidString;
+};
+
+module.exports = {
+    throwInvalidRange,
+    checkValue
+}
